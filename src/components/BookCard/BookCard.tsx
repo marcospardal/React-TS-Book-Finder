3a3a066ff30b9ab8aae0b6758e70d0e6d2e3722a
Pardal <marcosnascimento@eng.ci.ufpb.br>
2021-05-31 01:21:54 -0300

    :lipstick: Card animation

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Grid, Paper, makeStyles } from '@material-ui/core';
-import { Book } from '../../types';
+import { Book } from '../../store/ducks/books/types';
 
 const useStyles = makeStyles(() => ({
     card: {
         borderRadius: '20px',
         backgroundColor: 'blue',
         padding: '10px',
-        minHeight: '200px',
-        maxHeight: '200px',
+        minHeight: '250px',
+        maxHeight: '250px',
         minWidth: '180px',
+        transition: 'transform 0.15s ease-in-out',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-end',
         cursor: 'pointer',
+        backgroundSize: 'cover', 
+        textOverflow: 'ellipsis',
+    },
+    cardHovered: {
+        transform: 'scale3d(1.05, 1.05, 1)'
     },
     bookTitle: {
         margin: 0,
@@ -35,6 +41,10 @@ interface BookCardProps {
 
 function BookCard(props: BookCardProps) {
     const classes = useStyles();
+    const [cardState, setCardState] = React.useState({
+        raised: false,
+        shadow: 1
+    })
     const { book } = props;
 
 
@@ -46,8 +56,11 @@ function BookCard(props: BookCardProps) {
         <Grid item xs>
             <Paper
                 variant='elevation'
+                classes={{root: cardState.raised ? classes.cardHovered : ''}}
                 className={classes.card}
-                style={{ backgroundImage: `url(${book.imageLinks?.thumbnail ?? ''})`, backgroundSize: 'cover', textOverflow: 'ellipsis' }}
+                onMouseOver={() => setCardState({raised: true, shadow: 3})} onMouseLeave={() => setCardState({raised: false, shadow: 1})}
+                style={{ 
+                    backgroundImage: `url(${book.imageLinks?.thumbnail ?? ''})`, zIndex: cardState.shadow }}
             >
                 <p className={classes.bookTitle}>{book.title},{book.subtitle}</p>
                 <p style={{ margin: '2px 0px', fontFamily: 'Raleway', color: 'white', fontSize: '18px', textShadow: '2px 2px 0px black', backgroundColor: 'rgba(117,117,117 ,0.5)' }}>{book.authors}</p>
